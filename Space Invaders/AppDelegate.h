bb23542c4ed171a405083b8c4c00be241c7232e9
Jigar Revar <jigarrevar@Darshan-Revars-Mac-mini.local>
2014-07-31 12:53:05 -0700

    Initial Commit

---
+++ Space Invaders/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Space Invaders
+//
+//  Created by Jigar Revar on 7/31/14.
+//  Copyright (c) 2014 Jigar Revar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Space Invaders/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Space Invaders
+//
+//  Created by Jigar Revar on 7/31/14.
+//  Copyright (c) 2014 Jigar Revar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
